173bab38a1505ca3669df811643ea3c345b97b22
Christian Plattner <ccpp@gmx.at>
2016-06-12 16:50:56 +0200

    Bugfix: Move showitem configuration to ext_tables

--- Configuration/TCA/Overrides/tx_news_domain_model_news.php
+++ Configuration/TCA/Overrides/tx_news_domain_model_news.php
@@ -1,7 +1,6 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('tx_news_domain_model_news', 'tx_dinnerclub_notification_emails', '60', 'after:bodytext');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tx_news_domain_model_news', array(
 	'tx_dinnerclub_notification_emails' => array(
 		'exclude' => 1,
--- ext_tables.php
+++ ext_tables.php
@@ -2,3 +2,5 @@
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Dinnerclub');
 
+// This does always not work in Configuration/TCA/Overrides/tx_news_domain_model_news
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('tx_news_domain_model_news', 'tx_dinnerclub_notification_emails', '60', 'after:bodytext');
