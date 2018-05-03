90d418f015be1a6793fe2edeba00b1f7f07057fa
Christian Plattner <ccpp@gmx.at>
2018-05-03 16:55:05 +0200

    Add template for sending newsletter mails

--- ext_tables.php
+++ ext_tables.php
@@ -1,6 +1,7 @@
 <?php
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Dinnerclub');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/DirectMail', 'Dinnerclub Newsletter');
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
 	$_EXTKEY,
