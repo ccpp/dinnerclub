5db07b9c17efdf08bb45a0859b3fdc3fb8ffa02b
Christian Plattner <Christian.Plattner@world-direct.at>
2018-08-01 11:00:19 +0200

    Fix compatibility with TYPO3 7

--- Classes/Domain/Repository/RegistrationRepository.php
+++ Classes/Domain/Repository/RegistrationRepository.php
@@ -5,7 +5,7 @@ use TYPO3\CMS\Extbase\Persistence\Repository;
 class RegistrationRepository extends Repository {
 
 	public function initializeObject() {
-		$querySettings = $this->objectManager->create('TYPO3\\CMS\\Extbase\\Persistence\\Generic\\Typo3QuerySettings');
+		$querySettings = $this->objectManager->get('TYPO3\\CMS\\Extbase\\Persistence\\Generic\\Typo3QuerySettings');
 		$querySettings->setRespectStoragePage(FALSE);
 		$this->setDefaultQuerySettings($querySettings);
 	}
