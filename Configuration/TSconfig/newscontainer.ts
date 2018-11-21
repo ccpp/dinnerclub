1859dafc49541d0c50b5fbcf1905d3d2875e2537
Christian Plattner <ccpp@gmx.at>
2018-11-21 23:03:57 +0100

    Change order of tables in list module

--- Configuration/TSconfig/newscontainer.ts
+++ Configuration/TSconfig/newscontainer.ts
@@ -5,3 +5,6 @@ mod.web_listcal {
 TCAdefaults.tx_news_domain_model_news {
     type = 60
 }
+mod.web_list.tableDisplayOrder.tx_dinnerclub_domain_model_registration {
+    after = tx_news_domain_model_news
+}
