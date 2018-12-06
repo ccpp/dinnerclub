b44787cebdd90eecdce129f5b5d2a90407982e1a
Christian Plattner <ccpp@gmx.at>
2018-12-06 16:39:02 +0100

    Make backend editing of news easier

--- Configuration/TSconfig/news.ts
+++ Configuration/TSconfig/news.ts
@@ -7,3 +7,40 @@ tx_news.templateLayouts {
 	58 = Ab-/Ummeldungsformular
 }
 
+TCEFORM.tx_news_domain_model_news {
+	# General
+	istopnews.types.60.disabled = 1
+	sys_language_uid.types.60.disabled = 1
+	author.types.60.disabled = 1
+	author_email.types.60.disabled = 1
+	archive.types.60.disabled = 1
+
+	# Access
+	starttime.types.60.disabled = 1
+	endtime.types.60.disabled = 1
+	fe_group.types.60.disabled = 1
+	editlock.types.60.disabled = 1
+
+	# Options
+	categories.types.60.disabled = 1
+	tags.types.60.disabled = 1
+
+	# Relations
+	fal_media.types.60 {
+		label = Bild
+		disabled = 0
+	}
+	fal_related_files.types.60.disabled = 1
+	related_links.types.60.disabled = 1
+	related.types.60.disabled = 1
+	related_from.types.60.disabled = 1
+
+	# Metadata
+	keywords.types.60.disabled = 1
+	description.types.60.disabled = 1
+	alternative_title.types.60.disabled = 1
+	path_segment.types.60.disabled = 1
+
+	# Notes: is not an excludefield!
+	notes.types.60.disabled = 1
+}
