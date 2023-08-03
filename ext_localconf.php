d0e2819757b215929be2241835e6dbc8a56837e4
Christian Plattner <Christian.Plattner@mxr-tactics.com>
2023-08-03 23:40:07 +0200

    Changes from June 25 on webpage

--- ext_localconf.php
+++ ext_localconf.php
@@ -19,3 +19,16 @@
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] =
 	'CP\Dinnerclub\Controller\DinnerclubCommandController';
+
+// TODO og:image should be  1.9 : 1
+
+
+if (!$_SERVER['REQUEST_URI'] == '/browserconfig.xml') {
+	//$_SERVER['REQUEST_URI'] = '/';
+	//$_GET['type'] = 98;
+	#$_GET['eID'] = $_EXTKEY;
+
+	// Stop realurl:
+	$_SERVER['SCRIPT_NAME'] = '';
+	$_SERVER['PATH_INFO'] = '';
+}
